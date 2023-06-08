c80720d0baa17ed454a8ec712cd930a614de8172
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-06-09 00:48:31 +0400

    Finished First Lesson

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let js = "amazing";
-if (js === "amazing") alert("JS is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_mitalda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";

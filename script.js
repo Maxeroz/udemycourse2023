07e4b57143d264d30229668c0c48667d1aba5553
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-06-10 15:38:32 +0400

    Commention out code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+/*
 let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log(23);
@@ -10,11 +11,18 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let jonas_mitalda = "JM";
 let $function = 27;
 
 let person = "jonas";
 let PI = 3.1415;
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/

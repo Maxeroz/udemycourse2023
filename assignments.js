0bb4f112fa3ff9f088e588cca196b00bf07e24c5
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-06-10 22:32:29 +0400

    Finished lecture operators

--- assignments.js
+++ assignments.js
@@ -1,9 +1,21 @@
 /*
-let country = "Russia";
-let continent = "Eurasia";
+// Lecture: Values and Variables
+const country = "Russia";
+const continent = "Eurasia";
 let population = 145;
 
-console.log(country);
-console.log(continent);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Lecture: Data types
+
+const isIsland = false;
+let language = "Russian";;
+console.log(isIsland);
 console.log(population);
+console.log(country);
+console.log(language);
+// Lecture: let, const and var ;
+language = "Russian";
 */

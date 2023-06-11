04ecc1a6451577c3fe9bd3a0bda22e6fa6c2c475
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-06-12 01:08:08 +0400

    CHALLENGE #2

--- assignments.js
+++ assignments.js
@@ -1,4 +1,4 @@
-// /*
+/*
 // Lecture: Values and Variables
 const country = "Portugal";
 const continent = "Europe";
@@ -33,10 +33,25 @@ console.log(population > averagePopulation);
 
 const newDescription = `${country} is in Europe, and its ${population} millions people speak ${language}`;
 console.log(newDescription);
-// */
 
 if (population > 33) {
     console.log(`${country}'s population is above average.`);
 } else {
     console.log(`${country}'s population is ${averagePopulation - population} million below average`);
+}
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
